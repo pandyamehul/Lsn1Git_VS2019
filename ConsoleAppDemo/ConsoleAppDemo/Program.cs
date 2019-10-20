eec8c3b9739714a970f025394602deb34553a839
Mehul <pandya.mehul@live.com>
2019-10-20 13:16:33 +0200

    Commit change

--- ConsoleAppDemo/ConsoleAppDemo/Program.cs
+++ ConsoleAppDemo/ConsoleAppDemo/Program.cs
@@ -5,8 +5,9 @@ namespace ConsoleAppDemo
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Changed after commit");
         }
     }
 }
