4f841a6abcd08e10cc275ade9dc7408f5a6a0394
Mehul <pandya.mehul@live.com>
2019-10-20 13:36:00 +0200

    Added Feature 1 class and reference of feature in mail class i.e. Program.cs

---
+++ ConsoleAppDemo/ConsoleAppDemo/clsFeature1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppDemo
+{
+    class clsFeature1
+    {
+        public void printData()
+        {
+            Console.WriteLine("This is feature1");
+        }
+    }
+}
